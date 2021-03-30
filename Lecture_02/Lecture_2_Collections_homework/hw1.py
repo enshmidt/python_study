6ebcd273d6b8d432c7bcdd9ee0a06b686d067efd
Elena Shmidt <elena_shmidt@epam.com>
2021-03-30 23:03:40 +0300

    Just three parts of the 2nd homework

--- Lecture_02/Lecture_2_Collections_homework/hw1.py
+++ Lecture_02/Lecture_2_Collections_homework/hw1.py
@@ -29,15 +29,31 @@ foo@example.com 729.83 EUR accountName 2021-01:0 validate_date
 bar@example.com 729.83 accountName 2021-01-02 validate_line
 """
 from typing import Callable, Iterable
+import re
+import os
+
+filepath = 'data.txt'
 
 
 def validate_line(line: str) -> bool:
-    ...
+    """Check if the line consists of 5 elements"""
+    return len(line.split(" ")) == 5
 
 
 def validate_date(date: str) -> bool:
-    ...
+    """Check if the date is in format DDDD-DD-DD"""
+    checked_str = date.split(" ")[-1:]
+    return bool(re.fullmatch("\d{4}-\d{2}-\d{2}", *checked_str))
 
 
-def check_data(filepath: str, validators: Iterable[Callable]) -> str:
-    ...
+def check_data(file_path: str, validators: Iterable[Callable]) -> str:
+    with open(file_path, 'r') as data_file, open('output_file.txt', 'w+') as output_file:
+        for line in data_file:
+            line = line.strip()
+            for validator in validators:
+                if not validator(line):
+                    output_file.write(f"{line} {validator.__name__}\n")
+                    output_file.flush()
+                    break
+    return os.path.abspath('output_file.txt')
+
--- Lecture_02/Lecture_2_Collections_homework/hw2.py
+++ Lecture_02/Lecture_2_Collections_homework/hw2.py
@@ -21,4 +21,32 @@ from typing import List, Tuple
 
 
 def major_and_minor_elem(inp: List) -> Tuple[int, int]:
-    ...
+    return *major(inp), *minor(inp)
+
+
+def minor(inp: List):
+    """The least common element is the element that appears fewer than other."""
+    count_list = [inp.count(i) for i in inp]
+    min_value = min(count_list)
+    index_list = []
+    for ind, value in enumerate(count_list):
+        if value == min_value:
+            index_list.append(ind)
+    values = {inp[i] for i in index_list}
+    return values
+
+
+def major(inp: List):
+    """The most common element is the element that appears more than inp // 2 times."""
+    index_list = []
+    for index, value in enumerate(inp):
+        if inp.count(value) > len(inp)//2:
+            index_list.append(index)
+    values = [inp[i] for i in index_list]
+    return set(values)
+
+
+# inputV = [3, 2, 3]
+# inputV2 = [2,2,1,1,1,2,2]
+# inputV3 = [2,2,1,1,1,2,2,5,8,5,1,7,2,4]
+# major_and_minor_elem(inputV2)
