20909955e4350849b217609c615bec580d235b07
Elena Shmidt <koloboka@mail.ru>
2021-05-23 17:28:48 +0300

    unpack vars

--- Lecture_04/Lecture_4_Testing_homework/continued_fraction_task.py
+++ Lecture_04/Lecture_4_Testing_homework/continued_fraction_task.py
@@ -7,13 +7,10 @@ def continued_fraction(a, b):
     5. повторяем п. 2-4
     6. до тех пор, пока остаток от деления не будет равен 0
     """
-    num = a
-    denum = b
+    num, denum = a, b
     res = []
     while denum != 0:
         whole = num // denum
         res.append(whole)
-        dif = num % denum
-        num = denum
-        denum = dif
+        num, denum = denum, num % denum
     return res
